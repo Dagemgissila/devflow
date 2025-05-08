886b106d46e2ef32595c0477a34b36b738d36766
Dagemgisila <dagemgisila@gmail.com>
2025-05-08 09:49:17 +0300

    setup font

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <h1>Wel come to next js </h1>
+      <h1 className="h1-bold">Well come to next js </h1>
+      <h1 className="h1-bold font-space-grotesk">Well come to next js </h1>
     </div>
   );
 }
