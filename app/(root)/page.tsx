ea0c30b424ee67c58abfcdfc724523fe92531438
Dagemgisila <dagemgisila@gmail.com>
2025-05-11 22:06:02 +0300

    Leftsidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 export default async function Home() {
   const session = await auth();
@@ -9,15 +7,6 @@ export default async function Home() {
     <div>
       <h1 className="h1-bold">Well come to next js </h1>
       <h1 className="h1-bold font-space-grotesk">Well come to next js </h1>
-
-      <form
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button>Log out</Button>
-      </form>
     </div>
   );
 }
