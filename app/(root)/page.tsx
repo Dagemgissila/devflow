c599b2c07f0f50756f50d29a8c615b84aaa7cf63
Dagemgisila <dagemgisila@gmail.com>
2025-05-19 10:11:10 +0300

    implement log

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,9 @@ import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import handleError from "@/lib/handlers/error";
+import { ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongosee";
 
 const questions = [
   {
@@ -47,11 +50,21 @@ const questions = [
   },
 ];
 
+const test = async () => {
+  try {
+    await dbConnect();
+  } catch (error) {
+    return handleError(error);
+  }
+};
+
 interface SearchParams {
   searchParams: Promise<{ [key: string]: string }>;
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
+  await test();
+
   const { query = "", filter = "" } = await searchParams;
 
   const filteredQuestions = questions.filter((question) => {
