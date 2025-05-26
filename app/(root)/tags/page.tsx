45c87ce447565f13b54d6c445ef042e139226f37
Dagemgisila <dagemgisila@gmail.com>
2025-05-26 14:58:30 +0300

    implement get tags server action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,6 +1,14 @@
+import { getTags } from "@/lib/actions/tag.action";
 import React from "react";
 
-const Tags = () => {
+const Tags = async () => {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "react js",
+  });
+  const { tags } = data || {};
+  console.log(tags);
   return <div>Tags</div>;
 };
 
