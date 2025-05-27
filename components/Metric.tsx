32e9421b7455e4116a6a608ad7ec02b76ef092ec
Dagemgisila <dagemgisila@gmail.com>
2025-05-27 16:09:26 +0300

    fetch question detail

--- components/Metric.tsx
+++ components/Metric.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -9,7 +10,9 @@ interface Props {
   title: string;
   href?: string;
   textStyles: string;
+  imgStyles?: string;
   isAuthor?: boolean;
+  titleStyles?: string;
 }
 
 const Metric = ({
@@ -19,7 +22,9 @@ const Metric = ({
   title,
   href,
   textStyles,
+  imgStyles,
   isAuthor,
+  titleStyles,
 }: Props) => {
   const metricContent = (
     <>
@@ -28,17 +33,17 @@ const Metric = ({
         width={16}
         height={16}
         alt={alt}
-        className={`rounded-full object-contain  `}
+        className={`rounded-full object-contain ${imgStyles}`}
       />
 
       <p className={`${textStyles} flex items-center gap-1`}>
         {value}
 
-        <span
-          className={`small-regular line-clamp-1 ${isAuthor ? "max-sm:hidden" : ""}`}
-        >
-          {title}
-        </span>
+        {title ? (
+          <span className={cn(`small-regular line-clamp-1`, titleStyles)}>
+            {title}
+          </span>
+        ) : null}
       </p>
     </>
   );
