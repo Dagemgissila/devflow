c784d3b474237d6302a822066bf454ad206c2161
Dagemgisila <dagemgisila@gmail.com>
2025-05-28 14:34:25 +0300

    AI answer

--- components/forms/AnswerForm.tsx
+++ components/forms/AnswerForm.tsx
@@ -5,6 +5,7 @@ import { MDXEditorMethods } from "@mdxeditor/editor";
 import { ReloadIcon } from "@radix-ui/react-icons";
 import dynamic from "next/dynamic";
 import Image from "next/image";
+import { useSession } from "next-auth/react";
 import { useRef, useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -17,17 +18,25 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
-import { AnswerSchema } from "@/lib/validation";
-import { createAnswer } from "@/lib/actions/answer.action";
 import { toast } from "@/hooks/use-toast";
+import { createAnswer } from "@/lib/actions/answer.action";
+import { api } from "@/lib/api";
+import { AnswerSchema } from "@/lib/validation";
 
 const Editor = dynamic(() => import("@/components/editor"), {
   ssr: false,
 });
 
-const AnswerForm = ({ questionId }: { questionId: string }) => {
+interface Props {
+  questionId: string;
+  questionTitle: string;
+  questionContent: string;
+}
+
+const AnswerForm = ({ questionId, questionTitle, questionContent }: Props) => {
   const [isAnswering, startAnsweringTransition] = useTransition();
   const [isAISubmitting, setIsAISubmitting] = useState(false);
+  const session = useSession();
 
   const editorRef = useRef<MDXEditorMethods>(null);
 
@@ -39,16 +48,23 @@ const AnswerForm = ({ questionId }: { questionId: string }) => {
   });
 
   const handleSubmit = async (values: z.infer<typeof AnswerSchema>) => {
-    console.log(values);
-    const result = await createAnswer({ questionId, content: values.content });
-
     startAnsweringTransition(async () => {
+      const result = await createAnswer({
+        questionId,
+        content: values.content,
+      });
+
       if (result.success) {
         form.reset();
+
         toast({
           title: "Success",
-          description: "your answer has been posted succefully",
+          description: "Your answer has been posted successfully",
         });
+
+        if (editorRef.current) {
+          editorRef.current.setMarkdown("");
+        }
       } else {
         toast({
           title: "Error",
@@ -57,6 +73,60 @@ const AnswerForm = ({ questionId }: { questionId: string }) => {
         });
       }
     });
+  };
+
+  const generateAIAnswer = async () => {
+    if (session.status !== "authenticated") {
+      return toast({
+        title: "Please log in",
+        description: "You need to be logged in to use this feature",
+      });
+    }
+
+    setIsAISubmitting(true);
+
+    const userAnswer = editorRef.current?.getMarkdown();
+
+    try {
+      const { success, data, error } = await api.ai.getAnswer(
+        questionTitle,
+        questionContent,
+        userAnswer
+      );
+
+      if (!success) {
+        return toast({
+          title: "Error",
+          description: error?.message,
+          variant: "destructive",
+        });
+      }
+
+      const formattedAnswer = data.replace(/<br>/g, " ").toString().trim();
+
+      if (editorRef.current) {
+        editorRef.current.setMarkdown(formattedAnswer);
+
+        form.setValue("content", formattedAnswer);
+        form.trigger("content");
+      }
+
+      toast({
+        title: "Success",
+        description: "AI generated answer has been generated",
+      });
+    } catch (error) {
+      toast({
+        title: "Error",
+        description:
+          error instanceof Error
+            ? error.message
+            : "There was a problem with your request",
+        variant: "destructive",
+      });
+    } finally {
+      setIsAISubmitting(false);
+    }
   };
 
   return (
@@ -68,11 +138,12 @@ const AnswerForm = ({ questionId }: { questionId: string }) => {
         <Button
           className="btn light-border-2 gap-1.5 rounded-md border px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
           disabled={isAISubmitting}
+          onClick={generateAIAnswer}
         >
           {isAISubmitting ? (
             <>
               <ReloadIcon className="mr-2 size-4 animate-spin" />
-              Generating...isAISubmitting
+              Generating...
             </>
           ) : (
             <>
