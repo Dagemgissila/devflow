79ce4908c2c0848cda5ac3d8bb4372e3c147652a
Dagemgisila <dagemgisila@gmail.com>
2025-05-30 13:59:50 +0300

    implement get users actions

--- lib/actions/vote.action.ts
+++ lib/actions/vote.action.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import mongoose, { ClientSession } from "mongoose";
+import { revalidatePath } from "next/cache";
 
+import ROUTES from "@/constants/routes";
 import { Answer, Question, Vote } from "@/database";
 
 import action from "../handlers/action";
@@ -11,8 +13,6 @@ import {
   HasVotedSchema,
   UpdateVoteCountSchema,
 } from "../validation";
-import { revalidatePath } from "next/cache";
-import ROUTES from "@/constants/routes";
 
 export async function updateVoteCount(
   params: UpdateVoteCountParams,
@@ -66,7 +66,7 @@ export async function createVote(
   const { targetId, targetType, voteType } = validationResult.params!;
   const userId = validationResult.session?.user?.id;
 
-  if (!userId) handleError(new Error("Unauthorized")) as ErrorResponse;
+  if (!userId) return handleError(new Error("Unauthorized")) as ErrorResponse;
 
   const session = await mongoose.startSession();
   session.startTransaction();
@@ -92,6 +92,10 @@ export async function createVote(
           existingVote._id,
           { voteType },
           { new: true, session }
+        );
+        await updateVoteCount(
+          { targetId, targetType, voteType: existingVote.voteType, change: -1 },
+          session
         );
         await updateVoteCount(
           { targetId, targetType, voteType, change: 1 },
@@ -123,6 +127,7 @@ export async function createVote(
     session.endSession();
 
     revalidatePath(ROUTES.QUESTION(targetId));
+
     return { success: true };
   } catch (error) {
     await session.abortTransaction();
