49bcb6feff07f0dce4c6d45e824814b995464b46
Dagemgisila <dagemgisila@gmail.com>
2025-06-03 15:32:42 +0300

    make home page size 10

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -21,7 +21,7 @@ const Home = async ({ searchParams }: SearchParams) => {
 
   const { success, data, error } = await getQuestions({
     page: Number(page) || 1,
-    pageSize: Number(pageSize) || 2,
+    pageSize: Number(pageSize) || 10,
     query: query || "",
     filter: filter || "",
   });
